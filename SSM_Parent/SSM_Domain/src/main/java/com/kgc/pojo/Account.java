413bbf49873e76f9cfb48c1dd65c2b59cd6ca1d0
yyq <1786262@qq.com>
2020-08-27 11:33:39 +0800

    xian

---
+++ SSM_Parent/SSM_Domain/src/main/java/com/kgc/pojo/Account.java
@@ -0,0 +1,55 @@
+package com.kgc.pojo;
+
+import java.io.Serializable;
+
+/**
+ * 账户的实体类
+ */
+public class Account implements Serializable {
+
+    private int id;
+    private String name;
+    private double money;
+
+    public Account() {
+    }
+
+    public Account(int id, String name, double money) {
+        this.id = id;
+        this.name = name;
+        this.money = money;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", money=" + money +
+                '}';
+    }
+}
